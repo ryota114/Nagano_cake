c6d6665121994dd5d017cf5d8c0baf8d6ac27995
ryota114 <namahann0978@gmail.com>
2021-04-11 05:48:18 +0000

    [Add]admin,publicのtop,aboutのview

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # namespaceを使用、pubilcのURLにはpublicを表示したくないので、scope modul:で実装
+  scope modul: :public do
+    root to: "public/homes#top"
+    get "/about" => "public/homes#about"
+  end 
+  
+  # namespaceを使用、adminのURLにはadminを表示するので、scopeで実装
+  scope :admin do
+    root to: "admin/homes#top"
+    
+  end 
 end
