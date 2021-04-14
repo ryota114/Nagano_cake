56aa693b12c5cc5a9bd4758a408c2ea7fcefe93e
ryota114 <namahann0978@gmail.com>
2021-04-14 12:46:06 +0000

    [Update]headerの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  # adminとcustomerのdeviseを作成,アクションがかぶるので下記設定で
-  devise_for :admins, controllers: {
-    sessions: 'admins/sessions',
-    passwords: 'admins/passwords',
-    registrations: 'admins/registrations'
+  # adminとcustomerのdeviseを作成,アクションがかぶるので下記設定で整えた
+  devise_for :admin, controllers: {
+    sessions: 'admin/sessions',
+    passwords: 'admin/passwords',
+    registrations: 'admin/registrations'
   }
+  
   devise_for :customers, controllers: {
     sessions: 'customers/sessions',
     passwords: 'customers/passwords',
@@ -15,11 +16,19 @@ Rails.application.routes.draw do
   scope modul: :public do
     root to: "public/homes#top"
     get "/about" => "public/homes#about"
+    # get "/items" => "public/items#index"
+    resources :items, only: [ :index,:show ], module: :public
+    resources :customers, only: [ :show, :edit, :update ], module: :public
+    resources :cart_items, only: [ :index, :update ], module: :public
   end 
   
   # namespaceを使用、adminのURLにはadminを表示するので、namespaceで実装
   namespace :admin do
     root to: "homes#top"
+    resources :items, only: [ :index, :new, :create, :show, :edit, :update ]
+    resources :customers, only: [ :index, :show, :edit, :update ]
+    resources :orders, only: [ :show, :update ]
+    resources :genres, only: [ :index, :create, :edit, :update ]
     
   end 
 end
