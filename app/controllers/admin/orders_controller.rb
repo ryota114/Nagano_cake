0e721c49090415a51186cef0fbb34f0c05fef6a9
ryota114 <namahann0978@gmail.com>
2021-04-17 16:27:19 +0000

    [Update]注文履歴詳細の注文ステータス作成

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,7 +1,18 @@
 class Admin::OrdersController < ApplicationController
   def show
+    @order = Order.find(params[:id])
   end 
   
   def update
+    @order = Order.find(params[:id])
+    @order.update(order_params)
+    redirect_to admin_order_path(params[:id])
+  end 
+  
+  private
+  
+  # updateアクションでstatusの変更を受け付けるために実装
+  def order_params
+    params.require(:order).permit(:status)
   end 
 end
