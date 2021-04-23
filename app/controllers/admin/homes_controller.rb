294dc403778572cf713cef7360db60a2a03e85dd
ryota114 <namahann0978@gmail.com>
2021-04-23 02:00:48 +0000

    [Update]注文一覧遷移設定

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -1,6 +1,24 @@
 class Admin::HomesController < ApplicationController
   def top
-    # ページ(10個指定)の数を超えると次のページに表示する
-    @orders = Order.page(params[:page]).reverse_order
+    
+    # 遷移元の内容を取得
+    path = Rails.application.routes.recognize_path(request.referer)
+    
+    # ヘッダーからアクセスした場合
+    if params[:is_from_header] == "1"
+      @orders = Order.page(params[:page]).reverse_order
+      
+    # それ以外（カスタマー詳細画面から遷移や戻る操作）
+    else
+      # カスタマー詳細画面からの遷移を分岐、pathのコントローラーとアクションで認識
+      if path[:controller] == "admin/customers" && path[:action] == "show"
+        @customer = Customer.find(params[:customer_id])
+        @orders = @customer.orders.page(params[:page]).reverse_order
+      
+      # 戻る操作などによるエラー対策
+      else
+        @orders = Order.page(params[:page]).reverse_order
+      end 
+    end 
   end 
 end
