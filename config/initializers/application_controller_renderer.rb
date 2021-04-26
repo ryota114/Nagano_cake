1c6b3d5e62d2f5b80d6958282c030caac29978af
ryota114 <namahann0978@gmail.com>
2021-04-26 13:58:42 +0000

    [Update]admin/items/new新規作成画面

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '335a979bb827bda6d4a0ed8525df307cbe0385688fff4bfd19b5c8b0686289dad6f2ed61a431d7dc1b79d62689852ed35f330e2bba34032f1839eed5cd345233'
